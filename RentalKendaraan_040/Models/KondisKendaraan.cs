3314d99b8e95a9838c0a425260d916145790b960
Widyaputriheso <62939137+Widyaputriheso@users.noreply.github.com>
2020-10-25 20:37:28 +0700

    Add project files.

---
+++ RentalKendaraan_040/Models/KondisKendaraan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_040.Models
+{
+    public partial class KondisKendaraan
+    {
+        public int IdKondisi { get; set; }
+        public string NamaKondisi { get; set; }
+
+        public Pengembalian Pengembalian { get; set; }
+    }
+}
---
+++ RentalKendaraan_040/Models/Peminjaman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_040.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjamam { get; set; }
+        public DateTime? TglPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCustomer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public Jaminan IdCustomerNavigation { get; set; }
+        public Kendaraan IdPeminjamam1 { get; set; }
+        public Customer IdPeminjamamNavigation { get; set; }
+        public Pengembalian Pengembalian { get; set; }
+    }
+}
